28f1f9d14398668206a6ae9cb2fb7f407eed2a11
Mihke <mihkelviirsalu@hotmail.com>
2022-11-14 14:18:48 +0200

    Develop commit

--- src/main/java/ee/j6mmu/back_j6mmu/HelloWorldController.java
+++ src/main/java/ee/j6mmu/back_j6mmu/HelloWorldController.java
@@ -1,2 +1,14 @@
-package ee.j6mmu.back_j6mmu;public class HelloWorldController {
+package ee.j6mmu.back_j6mmu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/World")
+    public String helloWorld(){
+        return "HELLOOOO!!!!";
+
+    }
 }
